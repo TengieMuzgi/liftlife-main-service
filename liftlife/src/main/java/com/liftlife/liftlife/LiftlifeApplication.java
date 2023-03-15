6aa700f848b196486936b8f452616f06c8361bf3
mikkelx <michalss1115@gmail.com>
2023-03-15 18:56:05 +0100

    init

---
+++ liftlife/src/main/java/com/liftlife/liftlife/LiftlifeApplication.java
@@ -0,0 +1,13 @@
+package com.liftlife.liftlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftlifeApplication.class, args);
+	}
+
+}
---
+++ liftlife/src/test/java/com/liftlife/liftlife/LiftlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftlife.liftlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
