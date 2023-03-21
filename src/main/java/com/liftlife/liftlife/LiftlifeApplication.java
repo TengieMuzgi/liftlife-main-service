fa753792afcb2e51489068e4d09ff044f80f7d86
mikkelx <michalss1115@gmail.com>
2023-03-21 15:44:45 +0100

    Docker build test

--- src/main/java/com/liftlife/liftlife/LiftlifeApplication.java
+++ src/main/java/com/liftlife/liftlife/LiftlifeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiftlifeApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LiftlifeApplication.class, args);
 	}
