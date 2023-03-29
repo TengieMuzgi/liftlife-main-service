233ff9ea2297d332d037a95143fbd18f101eca1a
tzapadlinski <tomasz1129@gmail.com>
2023-03-29 15:58:29 +0200

    another test for pull request pipeline

--- src/main/java/com/liftlife/liftlife/DB.java
+++ src/main/java/com/liftlife/liftlife/DB.java
@@ -15,21 +15,21 @@ public class DB {
     private final DatabaseReference ref;
 
     public DB() throws IOException {
-        FileInputStream refreshToken = new FileInputStream("src/main/resources/liftlife-b89d1-firebase-adminsdk-1v4ox-c62b51cb58.json");
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/liftlife-b89d1-firebase-adminsdk-1v4ox-c62b51cb58.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://liftlife-b89d1-default-rtdb.europe-west1.firebasedatabase.app")
                 .build();
 
         FirebaseApp.initializeApp(options);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        ref = database.getReference("messages");
+        ref = database.getReference("test");
     }
 
     @PostConstruct
     private void init(){
-        ref.child("messages").setValueAsync("Hello, I'm working properly!");
+        ref.setValueAsync("Hello, I'm working properly!");
     }
 }
