b0be062a8a99411878b5832bb5779a8bb63412a2
mikkelx <michalss1115@gmail.com>
2023-04-17 23:55:44 +0200

    Exception handler, trainings moved to service, Repository template rework

--- src/main/java/com/liftlife/liftlife/training/TrainingRepository.java
+++ src/main/java/com/liftlife/liftlife/training/TrainingRepository.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutionException;
 @Repository
 @DependsOn("firestoreConnector")
 public class TrainingRepository extends FirestoreRepositoryTemplate<Training> {
-    public TrainingRepository(FirestoreMapper firestoreMapper) {
+    public TrainingRepository() {
         super(Training.class);
     }
 
