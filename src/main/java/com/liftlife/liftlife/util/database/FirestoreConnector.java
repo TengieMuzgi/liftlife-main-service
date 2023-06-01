2e77954b6bcb474b26357e03093880d6c5608282
tzapadlinski <tomasz1129@gmail.com>
2023-06-01 20:36:36 +0200

    Added missing connector

---
+++ src/main/java/com/liftlife/liftlife/util/database/FirestoreConnector.java
@@ -0,0 +1,26 @@
+package com.liftlife.liftlife.util.database;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class FirestoreConnector {
+
+    public FirestoreConnector() throws IOException {
+        InputStream serviceAccount = getClass()
+                .getResourceAsStream("/liftlife-firebase-adminsdk-connection-string.json");
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+    }
+
+}
