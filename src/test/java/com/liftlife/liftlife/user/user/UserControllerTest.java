5b9d6df5ea60340be161cf83cbd5e8b62e21d413
mikkelx <michalss1115@gmail.com>
2023-06-04 20:02:06 +0200

    New tests for user

--- src/test/java/com/liftlife/liftlife/user/user/UserControllerTest.java
+++ src/test/java/com/liftlife/liftlife/user/user/UserControllerTest.java
@@ -11,7 +11,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
 import java.util.Date;
