5e7b224c4ceb05108585668871ca335b0e371973
Tomasz Zapadliński <79110657+tzapadlinski@users.noreply.github.com>
2023-06-05 08:46:50 +0200

    Training validation (#37)
    * applied some changes due to class diagram changes and instructions on api for frontend
    
    * applied some changes due to class diagram changes and instructions on api for frontend
    
    * Finished working with serialization, deserialization, attribute list and done first endpoints for trainings, mainly insert/find.
    
    * Added validation to fields and TrainingActivity, optimised imports :D
    
    * More validation

--- src/main/java/com/liftlife/liftlife/trainingModule/trainingDay/TrainingDayController.java
+++ src/main/java/com/liftlife/liftlife/trainingModule/trainingDay/TrainingDayController.java
@@ -5,10 +5,15 @@ import com.liftlife.liftlife.trainingModule.trainingSession.TrainingSession;
 import com.liftlife.liftlife.trainingModule.trainingSession.TrainingSessionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/trainings/days")
@@ -56,6 +61,19 @@ public class TrainingDayController {
             @RequestBody @Valid TrainingDay trainingDay,
             @RequestParam String planId) {
         return ResponseEntity.ok(trainingDayService.updateDay(trainingDay, planId));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 
 }
